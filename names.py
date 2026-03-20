d596ba28615ada1b88481b43c52d7623bb14ed06
pauliAlonso <paulialonso33@gmail.com>
2026-03-19 21:34:50 -0300

    Update names.py

--- names.py
+++ names.py
@@ -2,12 +2,11 @@ def names():
     nombre = input("Ingrese su nombre:")
     apellido = input("Ingrese su apellido:")
 
-    nombre_completo = nombre + apellido 
+    nombre_completo = nombre + " " + apellido 
 
     print(nombre_completo.lower())
     print(nombre_completo.title())
     print(nombre_completo.upper())
-    print(\t nombre_completo.lower()) 
- 
+    print(f"\t{nombre_completo.lower()}") 
 
  
