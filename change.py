f49c10fbae293331af8e8d768840c9afff54feaa
pauliAlonso <paulialonso33@gmail.com>
2026-03-19 21:58:41 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,25 +1,27 @@
 def change():
-
-    gasto = float(input("Ingresar gasto:\n"))
-    dinero_recibido = int(input("Dinero recibido:\n"))
-
-    vuelto = dinero_recibido - gasto 
-
-    pesos = int(vuelto) 
-    centavos = round(vuelto - pesos ) * 100 
-  
     print("Ingresar gasto:")
-    print(gasto)
-    print("Dinero recibido")
-    print(dinero_recibido)
+    gasto = float(input())
 
-    print(vuelto)
+    print(gasto)
+
+    print("Dinero recibido")
+    dinero = float(input())
+
+    print(int(dinero) if dinero.is_integer() else dinero)
+
+    print()
+    print("Vuelto")
+    print()
+
+    vuelto = dinero - gasto
+
+    pesos = int(vuelto)
+    centavos = int(round((vuelto - pesos) * 100))
 
     print("Pesos:")
-    print(pesos) 
-    print("Centavos:") 
-    print(centavos) 
-    
+    print(pesos)
+    print("Centavos:")
+    print(centavos)    
 
     
 
